1f51af0a0d004471dcc33b101a7e9dac56c969a6
NoXPhasma <noxphasma@live.de>
2021-11-07 20:14:56 +0100

    Add BDA Xbox ONE Core controller

--- transport/wired.c
+++ transport/wired.c
@@ -526,6 +526,7 @@ static const struct usb_device_id xone_wired_id_table[] = {
 	{ XONE_WIRED_VENDOR(0x0f0d) }, /* Hori */
 	{ XONE_WIRED_VENDOR(0x1532) }, /* Razer */
 	{ XONE_WIRED_VENDOR(0x24c6) }, /* PowerA */
+	{ XONE_WIRED_VENDOR(0x20d6) }, /* BDA */
 	{ XONE_WIRED_VENDOR(0x044f) }, /* Thrustmaster */
 	{ XONE_WIRED_VENDOR(0x10f5) }, /* Turtle Beach */
 	{ XONE_WIRED_VENDOR(0x2e24) }, /* Hyperkin */
