238840183c6d5bcbb3cba128571ec5a361a9c6bc
Bennett Hardwick <me@bennetthardwick.com>
2024-02-12 02:59:22 +1000

    Add Priferential vendor ID

--- transport/wired.c
+++ transport/wired.c
@@ -561,6 +561,7 @@ static const struct usb_device_id xone_wired_id_table[] = {
 	{ XONE_WIRED_VENDOR(0x3537) }, /* GameSir */
 	{ XONE_WIRED_VENDOR(0x11c1) }, /* ??? */
 	{ XONE_WIRED_VENDOR(0x294b) }, /* Snakebyte */
+	{ XONE_WIRED_VENDOR(0x2c16) }, /* Priferential */
 	{ },
 };
 
